bc244a48ac1e65e1eb407a1a82683b927ccc29ff
esinomukete <esinomukete73@gmail.com>
2026-07-10 17:11:33 +0100

    Second Output learning

---
+++ output2.js
@@ -0,0 +1,13 @@
+console.log("Hello! I'm your coding fun fact guide!");
+let botName="Witwihcky";
+let botLocation="22 street";
+let favoriteLanguage="Java";
+console.log("My name is"+" "+botName+" "+"and I live on"+" "+botLocation+".");
+console.log("My favorite programming language is"+" "+ favoriteLanguage+".");
+let codingFact=("I love coding for Autobot with my best language"+" "+favoriteLanguage);
+console.log(codingFact);
+ codingFact=("I hate coding for Decepticons with my best language"+" "+favoriteLanguage);
+ console.log(codingFact);
+  codingFact=("I love coding with my best language"+" "+favoriteLanguage)
+  console.log(codingFact);
+  console.log("It was fun sharing these facts with you. Goodbye! - "+botName+" "+"from" +" "+ botLocation+".")
