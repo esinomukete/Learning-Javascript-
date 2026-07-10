943ec5802514e71192e19bb05bdd9af085bf4f71
esinomukete <esinomukete73@gmail.com>
2026-07-10 14:08:40 +0100

    Variable output
    Learning how to output variable content in Javascript

---
+++ index.js
@@ -0,0 +1,21 @@
+let Age=23;
+console.log(Age);
+
+const maxi=100;
+console.log(maxi);
+
+let freefire="Hello My name is Esino";
+console.log(freefire);
+
+let firstname="Esino";
+let lastname="Mukete";
+
+let fullname = firstname+ " " + lastname
+console.log(fullname);
+
+let great="Trying";
+great+=", to be the best"
+console.log(great);
+
+let name="Alice";
+console.log("Hello,"+" " + name + "!")
