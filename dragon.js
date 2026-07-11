34be25fb3be6e5e6247319504b6ad37a4398ed56
esinomukete <esinomukete73@gmail.com>
2026-07-11 14:48:09 +0100

    Add files via upload

---
+++ dragon.js
@@ -0,0 +1,23 @@
+let adjective="fierce";
+let noun="dragon";
+let verb="angry";
+let place="volcano";
+let adjective2="smoky";
+let noun2="tacos";
+
+let firstStory;
+firstStory="Once upon a time, there was a(n) "+ adjective+" "+noun+" "+"who loved to eat"+" "+noun2+"."+" "+"The"+" "+noun+" "+"lived in a"+" "+place+" "+"and had"+" "+adjective2+" "+"nostrils that blew fire when it was"+" "+verb+".";
+console.log("First story: "+firstStory);
+
+adjective="Wicked"
+noun="Decepticon"
+verb="evil"
+place="Cube"
+adjective2="Kind"
+noun2="Autobot"
+let secondStory;
+secondStory="Once upon a time, there was a(n) "+ adjective+" "+noun+" "+"who loved to eat"+" "+noun2+"."+" "+"The"+" "+noun+" "+"lived in a"+" "+place+" "+"and had"+" "+adjective2+" "+"nostrils that blew fire when it was"+" "+verb+".";
+console.log("Second story: "+secondStory)
+
+
+
