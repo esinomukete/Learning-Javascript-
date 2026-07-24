50d30c70bf0120f12bcb25bd42287f0835a1ade2
esinomukete <esinomukete73@gmail.com>
2026-07-24 14:52:43 +0100

    Beginning numbers

---
+++ workingNumber.js
@@ -0,0 +1,67 @@
+let Ny=12;
+let My=1.3;
+let Ky=0.1332;
+
+console.log(typeof Ny)
+console.log(typeof My)
+
+let me= 1/0;
+console.log(me)
+
+const notANumber = 'hello world' / 2;
+console.log(notANumber); // NaN
+
+let x1=9
+let x2=33
+let x3=45
+
+let result 
+result1=x1+x2
+result2=x1+x3
+result3 =x3+x2
+console.log(result1)
+console.log(result2)
+console.log(result3)
+
+let dif=20+1
+console.log(dif)
+
+
+let R=x2%x1
+console.log(R)
+
+//The exponentiation operator, represented by a double asterisk (**)//
+R=x2**x3
+console.log(R)
+
+let resultx = '10' + 5;
+
+console.log(resultx); // 105
+console.log(typeof resultx); // string
+
+//This is known as type coercion//
+
+
+//small lab//
+const firstResult = 5 + 10;
+console.log(`5 + 10 = ${firstResult}`);
+
+const secondResult = "8" - 5;
+console.log(`8 - 5 = ${secondResult}`);
+
+const thirdResult = 1 + 5;
+console.log(`1 + 5 = ${thirdResult}`);
+
+const fourthResult = 0 + 8;
+console.log(`0 + 8 = ${fourthResult}`);
+
+const fifthResult = 10 * 2;
+console.log(`10 * 2 = ${fifthResult}`);
+
+const sixthResult = 0 + 22;
+console.log(`0 + 22 = ${sixthResult}`);
+
+
+
+
+
